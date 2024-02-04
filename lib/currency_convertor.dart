67f6d797fd003574311f46dcb3b9d653695d7543
Mohammed Hasan <rafeek.ahmed1968@gmail.com>
2024-02-04 20:55:02 +0530

    Updated stylings

--- lib/currency_convertor.dart
+++ lib/currency_convertor.dart
@@ -20,31 +20,39 @@ class CurrencyConvertorMaterialApp extends StatelessWidget {
               // color: Color.fromARGB(255, 48, 44, 255),
             ),
           ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Color.fromARGB(255, 255, 255, 255),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              cursorColor: Colors.white,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(176, 255, 255, 255),
+                  ),
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(50)), // set the border radius here
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                ),
+                hoverColor: Colors.white,
+                labelText: 'Enter the amount in USD',
+                labelStyle: TextStyle(
+                  color: Colors.white70,
+                ),
+                prefixIcon: Icon(
+                  Icons.monetization_on,
+                  color: Color.fromARGB(255, 225, 225, 225),
                 ),
               ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Color.fromARGB(176, 255, 255, 255),
-                ),
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
               ),
-              hoverColor: Colors.white,
-              labelText: 'Enter the amount in USD',
-              labelStyle: TextStyle(
-                color: Colors.white70,
-              ),
-              prefixIcon: Icon(
-                Icons.monetization_on,
-                color: Color.fromARGB(255, 225, 225, 225),
-              ),
-            ),
-            style: TextStyle(
-              color: Color.fromARGB(255, 255, 255, 255),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
             ),
           ),
         ],
